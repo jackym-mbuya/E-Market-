4aeec73c1a9e9f45a055fa375ddc0f6d979ef60c
Jackym Mbuya <jackimmbuya@gmail.com>
2026-03-04 14:34:46 +0300

    Fix Render PORT binding

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
+
 $page_title = 'Home';
 require_once 'config/config.php';
 require_once 'config/database.php';
